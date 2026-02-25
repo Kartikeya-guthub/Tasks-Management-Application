229055d5e14a813f0520b0a47e073a0044517368
Kartikeya <kartikeya9917@gmail.com>
2026-02-25 22:22:02 +0530

    fix: add ESLint config for backend

---
+++ backend/.eslintrc.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 2021,
+  },
+  rules: {
+    'no-console': 'off',
+    'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+  },
+};
